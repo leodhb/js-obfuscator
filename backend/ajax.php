40b144cdffc9cc39972487118feb8f03e3a3e3a3
Henrique Barbosa <henrikleod@gmail.com>
2022-01-21 18:15:12 -0300

    Removing PHP backend

--- backend/ajax.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once 'hunter.php';
-    $code = $_POST['code'];
-    $code_decoded = base64_decode($code);
-
-    $hunter = new HunterObfuscator($code_decoded); 
-    $obsfucated = $hunter->Obfuscate();
-
-    echo base64_encode($obsfucated);
